636e8709b2c696f113ad1b36c00b25bca3922406
Theodoros Vasileiadis <theo.vasileiadis.work@gmail.com>
2017-10-23 12:05:13 +0300

    Fix model persistence between page refreshes.

--- app.py
+++ app.py
@@ -10,20 +10,18 @@ from sklearn.cluster import MiniBatchKMeans as mbk
 app = Flask(__name__)
 app.secret_key = 'v3rys3cr3t'
 
-@app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    cluster_reps = dict()
-    absolute_doc_id_dict = dict()
-    tfidf_sparse = joblib.load('{}/topic_space.txt'.format(args.data_file_path))
-    kmodel = joblib.load('{}/kmodel.txt'.format(args.data_file_path))
-    dist_space = joblib.load('{}/dist_space.txt'.format(args.data_file_path))
     sgform = ScatterGatherForm()
-    n_clusters = len(kmodel.cluster_centers_)
+    # Initialize cluster select/view form dynamically depending on the number of clusters.
+    n_clusters = len(app.config['kmodel'].cluster_centers_)
     sgform.cluster_select.choices=[(i, 'cluster_{}'.format(i)) for i in range(n_clusters)]
     sgform.cluster_view.choices=[(i, 'cluster_{}'.format(i)) for i in range(n_clusters)]
 
-    for ind in range(tfidf_sparse.shape[0]):
+    cluster_reps = dict()
+    absolute_doc_id_dict = dict()
+
+    for ind in range(app.config['topic_space'].shape[0]):
         absolute_doc_id_dict[ind] = ind
     if request.method == 'POST':
         doc_ids = []
@@ -37,21 +35,21 @@ def index():
             abs_ind = 0
             tmp_dict = dict()
             for selected_cluster_id in selected_clusters:
-                for doc_id, cluster_id in enumerate(kmodel.labels_):
+                for doc_id, cluster_id in enumerate(app.config['kmodel'].labels_):
                     if cluster_id == int(selected_cluster_id):
                         tmp_dict[abs_ind] = absolute_doc_id_dict[doc_id]
-                        new_tfidf = sp.vstack((new_tfidf, tfidf_sparse[doc_id]),
+                        new_tfidf = sp.vstack((new_tfidf, app.config['topic_space'][doc_id]),
                                               format='csr')
-            tfidf_sparse = new_tfidf[1:]
+            app.config['topic_space'] = new_tfidf[1:]
             absolute_doc_id_dict = dict(tmp_dict)
-            kmodel = mbk(n_clusters=len(kmodel.cluster_centers_), max_iter=10,
+            app.config['kmodel'] = mbk(n_clusters=len(app.config['kmodel'].cluster_centers_), max_iter=10,
                          verbose=True)
-            kmodel.fit(tfidf_sparse)
-            dist_space = kmodel.transform(tfidf_sparse)
-            print(dist_space.shape)
-            for i in range(len(kmodel.cluster_centers_)):
+            app.config['kmodel'].fit(app.config['topic_space'])
+            app.config['dist_space'] = app.config['kmodel'].transform(app.config['topic_space'])
+            print(app.config['dist_space'].shape)
+            for i in range(len(app.config['kmodel'].cluster_centers_)):
                 terms = []
-                topic_id = kmodel.cluster_centers_[i].argsort()[::-1][0]
+                topic_id = app.config['kmodel'].cluster_centers_[i].argsort()[::-1][0]
                 term_ids = lda_model.get_topic_terms(topic_id, topn=3)
                 for term_id in term_ids:
                     terms.append(dictionary[term_id[0]])
@@ -64,7 +62,7 @@ def index():
         if 'cluster_view' in request.form:
             cluster_id = request.form['cluster_view']
             app.logger.debug('\n\n\nView: ' + cluster_id)
-            dist = dist_space[:, int(cluster_id)] # Distances of the documents form the cluster center.
+            dist = app.config['dist_space'][:, int(cluster_id)] # Distances of the documents form the cluster center.
             # Find the ids of the documents that belogn to this cluster.
             doc_ids = dist.argsort()[:50]
             for id in doc_ids:
@@ -72,9 +70,9 @@ def index():
                 links.append(link_dict[absolute_doc_id_dict[id]])
                 summaries.append(summary_dict[absolute_doc_id_dict[id]])
             sgform.cluster_select.data = None
-            for i in range(len(kmodel.cluster_centers_)):
+            for i in range(len(app.config['kmodel'].cluster_centers_)):
                 terms = []
-                topic_id = kmodel.cluster_centers_[i].argsort()[::-1][0]
+                topic_id = app.config['kmodel'].cluster_centers_[i].argsort()[::-1][0]
                 term_ids = lda_model.get_topic_terms(topic_id, topn=3)
                 for term_id in term_ids:
                     terms.append(dictionary[term_id[0]])
@@ -84,9 +82,9 @@ def index():
                                links=links, summaries=summaries, cluster_reps=cluster_reps,
                                select_list=list(sgform.cluster_select))
 
-    for i in range(len(kmodel.cluster_centers_)):
+    for i in range(len(app.config['kmodel'].cluster_centers_)):
         terms = []
-        topic_id = kmodel.cluster_centers_[i].argsort()[::-1][0]
+        topic_id = app.config['kmodel'].cluster_centers_[i].argsort()[::-1][0]
         term_ids = lda_model.get_topic_terms(topic_id, topn=3)
         for term_id in term_ids:
             terms.append(dictionary[term_id[0]])
@@ -104,4 +102,7 @@ if __name__ == '__main__':
     title_dict = joblib.load('{}/title_dict.txt'.format(args.data_file_path))
     link_dict = joblib.load('{}/link_dict.txt'.format(args.data_file_path))
     summary_dict = joblib.load('{}/summary_dict.txt'.format(args.data_file_path))
+    app.config['topic_space'] = joblib.load('{}/topic_space.txt'.format(args.data_file_path))
+    app.config['kmodel'] = joblib.load('{}/kmodel.txt'.format(args.data_file_path))
+    app.config['dist_space'] = joblib.load('{}/dist_space.txt'.format(args.data_file_path))
     app.run(debug=True)
