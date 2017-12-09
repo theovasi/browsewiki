ce3969f7795b9ec6398d9753606b660320cc6865
Theodoros Vasileiadis <theo.vasileiadis.work@gmail.com>
2017-12-10 00:52:51 +0200

    Make lemmatizer return most frequent token.

--- toolset/mogreltk.py
+++ toolset/mogreltk.py
@@ -1,7 +1,10 @@
-""" This module offers a number tools for processing modern Greek text. """
-import re
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+""" This module offers tools for processing modern Greek text."""
 import time
 import math
+import re
+from operator import itemgetter
 from nltk import sent_tokenize, word_tokenize
 from greek_stemmer import GreekStemmer
 
@@ -38,8 +41,8 @@ def tokenize(text, stopwords_file_path=None):
         Args:
             text (str): The text to be tokenized.
         Returns:
-            filtered_tokens: The original text as a list of tokens excluding stopwords,
-                punctuation and numbers.
+            filtered_tokens: The original text as a list of tokens excluding
+            stopwords, punctuation and numbers.
     """
     filtered_tokens = []
     if stopwords_file_path is not None:
@@ -52,12 +55,13 @@ def tokenize(text, stopwords_file_path=None):
         for sent in sent_tokenize(text)
         for word in word_tokenize(sent)
     ]
-    # Remove tokens that 1) do not contain letters 2) are stopwords 3) are punctuation
-    # 4) contain less than 3 letters.
+    # Remove tokens that 1) do not contain letters 2) are stopwords
+    # 3) are punctuation 4) contain less than 3 letters.
     for token in tokens:
-        if not re.search('^\d*$', token)\
-                and not (stopwords_file_path is not None and normalize(token) in stopwords)\
-                and not re.search('[\.,!;:\"\'-«»\.\.\.%]', token)\
+        if not re.search(r'^\d*$', token)\
+                and not (stopwords_file_path is not None
+                         and normalize(token) in stopwords)\
+                and not re.search(r'[\.,!;:\"\'-«»\.\.\.%]', token)\
                 and len(token) > 2:
             filtered_tokens.append(token)
 
@@ -74,81 +78,87 @@ def stem(text, stopwords_file_path=None):
     """
     stemmer = GreekStemmer()
     tokens = tokenize(text, stopwords_file_path)
-    stems = [stemmer.stem(normalize(token).upper()).lower()
+    stems = [stemmer.stem(token.upper()).lower()
              for token in tokens]
     return stems
 
 
 class Lemmatizer(object):
-    """ Offers lemmatization functionality by matching tokens to stems(many to one). """
+    """ Offers pseudo-lemmatization functionality for the Greek language.
+
+    When the lemmatizer is fit on a text collection, it creates a dictionary
+    that matches each unique stem to a list of tuples. Each tuple contains a
+    token and its frequency of occurence in the collection. To lemmatize a
+    word, the word is stemmed first and then the most frequent token that has
+    been matches to the word's stem is selected as its lemma.
+
+    """
 
     def __init__(self):
         self.lemma_dict = dict()
 
-    def fit(self, text_collection_iterable, stopwords_file_path=None):
-        """ Process a text collection and create a dictionary that matches stems to tokens.
+    def fit(self, text_collection_iterable,
+            stopwords_file_path=None, verbose=False):
+        """ Process a text collection and create a dictionary that matches
+            stems to tokens.
 
             Args:
-                text_collection_iterable (obj): A list, generator or other iterable object that
-                    returns documents in string format.
-                stopwords_file_path (str): The path to a file containing stopwords. Stopwords
-                    are ignored during the processing of the collection.
+                text_collection_iterable (obj): A list, generator or other
+                    iterable object that returns documents in string format.
+                stopwords_file_path (str): The path to a file containing
+                    stopwords. Stopwords are ignored during the processing of
+                    the collection.
 
         """
-        print('Fitting lemmatizer...')
-        stemmer = GreekStemmer()
+        if verbose:
+            print('Fitting lemmatizer...')
         n_docs = 0
 
         start_time = time.time()
         for doc in text_collection_iterable:
             words_tokenized = tokenize(doc, stopwords_file_path)
-            words_stemmed = \
-                [stemmer.stem(normalize(token).upper()).lower()
-                 for token in words_tokenized]
+            words_stemmed = stem(doc, stopwords_file_path)
             for index, word in enumerate(words_stemmed):
                 self.add(word, words_tokenized[index])
 
-            # Print some info.
             n_docs += 1
-            if n_docs % 2000 == 0:
-                print('Processed {} documents... - {} docs/s'.format(n_docs,
-                                                                     math.floor(n_docs / (time.time() - start_time))))
+            if verbose and n_docs % 2000 == 0:
+                print('Processed {} documents... - {} docs/s'.format
+                      (n_docs,
+                       math.floor(n_docs / (time.time() - start_time))))
 
-    def add(self, key, value):
+    def add(self, stem_, token):
         """ Add a matching of a token to a stem."""
-        if not key in self.lemma_dict:
-            self.lemma_dict[normalize(key)] = [normalize(value)]
-        elif not value in self.lemma_dict[key]:
-            self.lemma_dict[normalize(key)].append(normalize(value))
+        if stem_ not in self.lemma_dict:
+            self.lemma_dict[stem_] = [[token, 0]]
+        else:
+            token_list = [token_entry[0]
+                          for token_entry in self.lemma_dict[stem_]]
+            if token not in token_list:
+                self.lemma_dict[stem_].append([token, 0])
+            else:
+                for token_entry in self.lemma_dict[stem_]:
+                    if token_entry[0] == token:
+                        token_entry[1] += 1
 
     def get(self, key):
         """ Returns the list of tokens matched to a stem. """
-        assert len(self.lemma_dict) > 0
-        assert key in self.lemma_dict
         return self.lemma_dict[key]
 
     def stem2lemma(self, key):
-        """ Takes a stem as argument and returns a noun that has it as root.
+        """ Takes a stem as argument and returns a token that has it as root.
 
             Args:
                 key (str): The stem that will be converted to a lemma.
 
             Returns:
-                lemma (str): A noun lemma that has the given stem as root.
+                (str): The most frequent token matched to a specific stem
+
         """
 
-        noun_suffixes = ['ς', 'η', 'o', 'οι', 'α']
         tokens = self.get(key)
-        counter = Count(tokens)
-        for suffix in noun_suffixes:
-            for token in tokens:
-                matched_tokens = []
-                if normalize(token).endswith(suffix):
-                    matched_tokens.append(token)
-            if len(matched_tokens) > 0:
-                token_len = [len(token) for token in matched_tokens]
-                return matched_tokens[token_len.index(min(token_len))]
-        return tokens[0]
+        # Return the most frequent token.
+        return max(tokens, key=itemgetter(1))[0]  # Faster than lambda.
 
     def lemmatize(self, word):
         """ Converts a word to its noun form.
@@ -158,8 +168,10 @@ class Lemmatizer(object):
 
             Returns:
                 lemma (str): A noun version of the given word.
+
         """
 
         key = stem(word)[0]
         lemma = self.stem2lemma(key)
+
         return lemma
