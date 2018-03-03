64e440d3bdb217dc5c6ed13b4a9303ca63dba233
Theodoros Vasileiadis <theo.vasileiadis.work@gmail.com>
2018-03-03 18:13:08 +0200

    Fix corpus tests.

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -7,39 +7,40 @@ import joblib
 
 
 class CorpusTest(unittest.TestCase):
-    
+
     def setUp(self):
-        self.filepath_dict = dict()
+        self.filepaths = []
         for i in range(3):
             with open('file{}'.format(i), 'w+') as file_:
                 file_.write('This is a test string.')
-            self.filepath_dict[i] = 'file{}'.format(i)
+            self.filepaths.append('file{}'.format(i))
 
         with open('file3', 'w+') as file_:
             file_.write('This is a longer test string.')
-        self.filepath_dict[3] = 'file3'.format(i)
+        self.filepaths.append('file3')
 
-        joblib.dump(self.filepath_dict, 'filepath_dict')
+        joblib.dump(self.filepaths, 'filepaths')
 
-        self.corpus = Corpus('filepath_dict')
+        self.corpus = Corpus(self.filepaths)
 
     def test_corpus_document_generator(self):
         for doc in self.corpus.document_generator():
             self.assertTrue(doc == 'This is a test string.'
-                    or doc == 'This is a longer test string.')
+                            or doc == 'This is a longer test string.')
 
     def test_corpus_stats(self):
         stat_log = self.corpus.stats()
         self.assertEqual(stat_log['n_docs'], 4)
-        self.assertEqual(stat_log['max_doc_size'], 7) # Dot is counterd a word.
+        # Dot is counterd a word.
+        self.assertEqual(stat_log['max_doc_size'], 7)
         self.assertEqual(stat_log['min_doc_size'], 6)
         self.assertEqual(stat_log['avg_doc_size'], 6.2)
 
     def tearDown(self):
         for i in range(4):
             os.remove('file{}'.format(i))
-        os.remove('filepath_dict')
-            
+        os.remove('filepaths')
+
 
 if __name__ == '__main__':
     unittest.main()
