b6f43b89e20ff2b8bd55e4b06da302b1149cef05
Theodoros Vasileiadis <theo.vasileiadis.work@gmail.com>
2020-12-30 11:13:59 +0000

    Make setup more resilient

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ import shutil
 
 import re
 import nltk
+import glob
 from docopt import docopt
 from urllib import request
 from bs4 import BeautifulSoup as bs
@@ -35,36 +36,46 @@ def app_setup(language):
         '/'.join(['http:/', mirror, dump_name]))
     parsed_version_select_page = bs(version_select_page, 'html5lib')
 
+    available_versions = []
     for link in parsed_version_select_page.findAll('a'):
         result = re.search(r"[0-9]+", str(link))
         if result:
-            latest_version = result.group(0)
+            available_versions.append(result.group(0))
 
-    # Now the download link can be built.
-    if os.path.exists('data/' + '-'.join([dump_name, latest_version,
-                                          'pages-articles.xml.bz2'])):
-        print('Latest dump version already exists.')
-    else:
-        if os.path.exists('data'):
-            shutil.rmtree('data')
-        os.mkdir('data')
-        os.chdir('data')
-        os.system('wget --no-check-certificate '
-                  + '/'.join(['https:/',
-                              mirror, dump_name, latest_version,
-                              (dump_name+'-' + latest_version +
-                               '-pages-articles.xml.bz2')]))
-        os.chdir('../')
+    latest_version = available_versions[-1]
+    available_versions = available_versions[:-1]
+    while not glob.glob(r'data/' + dump_name + '-*-pages-articles.xml.bz2'):
+        latest_version = available_versions[-1]
+        available_versions = available_versions[:-1]
+
+        # Now the download link can be built.
+        if glob.glob(r'data/' + dump_name + '-.*-pages-articles.xml.bz2'):
+            print('Latest dump version already exists.')
+        else:
+            if os.path.exists('data'):
+                shutil.rmtree('data')
+            os.mkdir('data')
+            os.chdir('data')
+            os.system('wget --no-check-certificate '
+                      + '/'.join(['https:/',
+                                  mirror, dump_name, latest_version,
+                                  (dump_name+'-' + latest_version +
+                                   '-pages-articles.xml.bz2')]))
+            os.chdir('../')
+
+    dump_version = re.match(r'data/' + dump_name + '-(.*)-pages-articles.xml.bz2', glob.glob(r'data/' + dump_name + '-*-pages-articles.xml.bz2')[0]).group(1)
 
     # Extract the documents form the compressed XML with Wikiextractor.
     # Wikiextractor puts the documents in txt files of equal size.
-    if not os.path.exists('data/' + dump_name + '-' + latest_version):
+    if not os.path.exists('data/' + dump_name + '-' + dump_version):
         os.system('git clone https://github.com/attardi/wikiextractor')
-        os.system('python3 wikiextractor/WikiExtractor.py -s '
-                  '--filter_disambig_pages --lists --keep_tables '
-                  '--no-templates -o data/' + dump_name + '-' +
-                  latest_version + ' data/' + dump_name + '-' +
-                  latest_version + '-pages-articles.xml.bz2')
+        os.mkdir('data/' + dump_name + '-' + dump_version)
+        os.chdir('wikiextractor')
+        os.system('python3 -m wikiextractor.WikiExtractor '
+                  '--no-templates -o ../data/' + dump_name + '-' +
+                  dump_version + ' ../data/' + dump_name + '-' +
+                  dump_version + '-pages-articles.xml.bz2')
+        os.chdir('../')
         shutil.rmtree('wikiextractor')
     else:
         print('Latest dump version already extracted.')
@@ -72,7 +83,7 @@ def app_setup(language):
     # Format the documents so that there is one document per txt file and
     # also create a dataframe that matches the document's filepaths to their
     # titles. short summaries and web links.
-    wiki_format('data/' + dump_name + '-' + latest_version,
+    wiki_format('data/' + dump_name + '-' + dump_version, sub_size=500000,
                 output_file_path='appdata')
 
     # Generate the topic space and build all the models the app uses.
